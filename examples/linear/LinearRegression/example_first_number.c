0b87dc2f4d83d22807ba1fe60a8d1da5551e8d9a
Georgi Korchakov <georgikorchakov@gmail.com>
2018-12-14 14:59:40 +0200

    [Fix] Dataset mistake

--- examples/linear/LinearRegression/example_first_number.c
+++ examples/linear/LinearRegression/example_first_number.c
@@ -12,7 +12,7 @@
 
 int main(){
     // {{1,2,3}, {2,1,1}, {4,5,6}, {5,6,3}};
-    matrix_t* X = MatrixInit(4, 3);
+    matrix_t* X = matrix_init(4, 3);
     X->data[0][0] = 1;
     X->data[0][1] = 2;
     X->data[0][2] = 3;
@@ -23,21 +23,21 @@ int main(){
 
     X->data[2][0] = 4;
     X->data[2][1] = 5;
-    X->data[3][2] = 6;
+    X->data[2][2] = 6;
 
     X->data[3][0] = 5;
     X->data[3][1] = 6;
     X->data[3][2] = 3;
 
-    //X->Print(X, 4);
+    X->print(X, 4);
 
-    vector_t* y = VectorInit(4);
+    vector_t* y = vector_init(4);
     y->data[0] = 1;
     y->data[1] = 2;
     y->data[2] = 4;
     y->data[3] = 5;
     
-    //y->Print(y, 4);
+    y->print(y, 4);
 
     // Model
     linear_regression_t* model = linear_regression_init(0.001, 3);
