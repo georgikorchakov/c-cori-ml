3c868295477e533c18a51318ac58122b3829b9c9
Georgi Korchakov <georgikorchakov@gmail.com>
2018-12-22 00:07:41 +0200

    [Update] Cleaned and more usefull

--- examples/linear/LinearRegression/example_first_number.c
+++ examples/linear/LinearRegression/example_first_number.c
@@ -11,44 +11,44 @@
 #include "../../../src/linear/LinearRegression/LinearRegression.h"
 
 int main(){
-    // {{1,2,3}, {2,1,1}, {4,5,6}, {5,6,3}};
-    matrix_t* X = matrix_init(4, 3);
-    X->data[0][0] = 1;
-    X->data[0][1] = 2;
-    X->data[0][2] = 3;
+  // {{1,2,3}, {2,1,1}, {4,5,6}, {5,6,3}};
+  matrix_t* X = matrix_init(4, 3);
+  X->data[0][0] = 1;
+  X->data[0][1] = 2;
+  X->data[0][2] = 3;
 
-    X->data[1][0] = 2;
-    X->data[1][1] = 1;
-    X->data[1][2] = 1;
+  X->data[1][0] = 2;
+  X->data[1][1] = 1;
+  X->data[1][2] = 1;
 
-    X->data[2][0] = 4;
-    X->data[2][1] = 5;
-    X->data[2][2] = 6;
+  X->data[2][0] = 4;
+  X->data[2][1] = 5;
+  X->data[2][2] = 6;
 
-    X->data[3][0] = 5;
-    X->data[3][1] = 6;
-    X->data[3][2] = 3;
+  X->data[3][0] = 5;
+  X->data[3][1] = 6;
+  X->data[3][2] = 3;
 
-    X->print(X, 4);
+  //X->print(X, 4);
 
-    vector_t* y = vector_init(4);
-    y->data[0] = 1;
-    y->data[1] = 2;
-    y->data[2] = 4;
-    y->data[3] = 5;
-    
-    y->print(y, 4);
+  vector_t* y = vector_init(4);
+  y->data[0] = 1;
+  y->data[1] = 2;
+  y->data[2] = 4;
+  y->data[3] = 5;
 
-    // Model
-    linear_regression_t* model = linear_regression_init(0.001, 3);
-    //model->verbose = 1;
+  //y->print(y, 4);
 
-    double x[3] = {3, 4, 5};
+  // Model
+  linear_regression_t* model = linear_regression_init(0.001, 3);
+  model->verbose = 1;
 
-    printf("Prediction: %f\n", model->predict(model, x));
-	printf("Cost: %f\n", model->cost(model, X, y));
-	printf("Best: %f\n", model->fit(model, X, y));
-	printf("Prediction: %f\n", model->predict(model, x));
+  double x[3] = {3, 4, 5};
 
-    return 0;
+  printf("Prediction: %f\n", model->predict(model, x));
+  printf("Cost: %f\n", model->cost(model, X, y));
+  printf("Best: %f\n", model->fit(model, X, y));
+  printf("Prediction: %f\n", model->predict(model, x));
+
+  return 0;
 }
--- examples/linear/LinearRegression/example_random.c
+++ examples/linear/LinearRegression/example_random.c
@@ -11,21 +11,21 @@
 #include "../../../src/linear/LinearRegression/LinearRegression.h"
 
 int main(){
-    matrix_t* X = MatrixInit(100, 1);
-    X->FillRandom(X, 0, 10);
-    X->Print(X, 4);
+  matrix_t* X = matrix_init(100, 1);
+  X->fill_random(X, 0, 10);
+  X->print(X, 4);
 
-    vector_t* y = VectorInit(100);
-    VectorFillRandomUsingMatrix(y, X, 3); //TODO Make it usefull
-    y->Print(y, 4);
-    
-    // Model
-    linear_regression_t* model = linear_regression_init(0.001, 1);
-    //model->verbose = 1;
+  vector_t* y = vector_init(100);
+  vector_fill_random_using_matrix(y, X, 3); //TODO Make it usefull
+  y->print(y, 4);
 
-    printf("Score from fitting function = %f\n", model->fit(model, X, y));
-    printf("prediction on first line = %f\n", model->predict(model, X->data[0]));
-    printf("Cost = %f\n", model->cost(model, X, y));
+  // Model
+  linear_regression_t* model = linear_regression_init(0.001, 1);
+  model->verbose = 1;
 
-    return 0;
+  printf("Score from fitting function = %f\n", model->fit(model, X, y));
+  printf("prediction on first line = %f\n", model->predict(model, X->data[0]));
+  printf("Cost = %f\n", model->cost(model, X, y));
+
+  return 0;
 }
