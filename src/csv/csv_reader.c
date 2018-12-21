a57c5a1d78d40ffd79d0773d88d7b1d0f2c275c8
Georgi Korchakov <georgikorchakov@gmail.com>
2018-12-21 23:59:54 +0200

    [Fix] Dataset segmentation fault

--- src/csv/csv_reader.c
+++ src/csv/csv_reader.c
@@ -1,5 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
@@ -9,19 +11,52 @@
 int main(){
   matrix_t *X = NULL;
   vector_t *y = NULL;
-  read_csv("iris.data", X, y);
+  read_csv("iris.data", &X, &y);
+
+  X->print(X, 180);
+  y->print(y, 180);
 
   return 0;
 }
 
 int
-read_csv(char *file_name, matrix_t *X, vector_t *y){
+is_number (char *str)
+{
+  for(int i = 0; str[i] != '\0'; i++)
+    {
+      if (str[i] != '.' &&
+          (str[i] < '0' || str[i] > '9'))
+        {
+          return 0;
+        }
+    }
+
+  return 1;
+}
+
+int
+in_arr(char str_arr[100][40], char *str)
+{
+  for (int i = 0; i < 100; i++)
+    {
+      if ( strcmp(str_arr[i], str) == 0 )
+        {
+          return i;
+        }
+    }
+
+  return -1;
+}
+
+int
+read_csv(char *file_name, matrix_t **X, vector_t **y)
+{
   int fd;
   int close_err;
   int rv;
-    
+
   fd = open(file_name, O_RDONLY);
-  if(fd == -1) fputs("Open failed!\n", __stdoutp);
+  if(fd == -1) fputs("Open failed!\n", stdout);
 
   char buffer[1024];
   char value[1024];
@@ -33,16 +68,21 @@ read_csv(char *file_name, matrix_t *X, vector_t *y){
   int feature = 0;
   int first = 1;
 
+  // For strings in csv
+  char strings[100][40];
+  int count_str = 0;
+  int pos = 0;
+
+  // Count number of samples and number of features
   do
     {
       rv = read(fd, buffer, sizeof(buffer));
-      if(rv == -1) fputs("Read failed!\n", __stdoutp);
+      if(rv == -1) fputs("Read failed!\n", stdout);
 
       if(first == 1)
         {
           for(int i = 0; buffer[i] != '\n'; i++)
               if(buffer[i] == ',') ++features;
-          fprintf(__stdoutp, "%d\n", features);
           first = 0;
         }
 
@@ -53,13 +93,14 @@ read_csv(char *file_name, matrix_t *X, vector_t *y){
   while (rv == sizeof(buffer));
 
   close_err = close(fd);
-  if(close_err == -1) fputs("Close failed!\n", __stdoutp);
+  if(close_err == -1) fputs("Close failed!\n", stdout);
 
-  X = matrix_init(line+1, features -1);
-  y = vector_init(line+1);
+  // Initialize matrix and vector
+  *X = matrix_init(line, features -1);
+  *y = vector_init(line);
 
   fd = open(file_name, O_RDONLY);
-  if(fd == -1) printf("Open failed!\n");
+  if(fd == -1) fputs("Open failed!\n", stdout);
 
   line = 0;
   feature = 0;
@@ -67,7 +108,7 @@ read_csv(char *file_name, matrix_t *X, vector_t *y){
   do
     {
       rv = read(fd, buffer, sizeof(buffer));
-      if(rv == -1) fputs("Read failed!\n", __stdoutp);
+      if(rv == -1) fputs("Read failed!\n", stdout);
 
       for(int i = 0; i < rv; i++)
         {
@@ -79,41 +120,55 @@ read_csv(char *file_name, matrix_t *X, vector_t *y){
           else
             {
               value[value_pos] = '\0';
-              sscanf(value, "%f", &real_value);
 
-              //printf("%d --- %d\n", line, feature);
-              if(feature == features - 1)
+              if ( is_number(value) )
                 {
-                  y->data[line] = real_value;
+                  sscanf(value, "%f", &real_value);
                 }
               else
                 {
-                  X->data[line][feature] = real_value;
+                  pos = in_arr(strings, value);
+                  if (pos == -1)
+                    {
+                      strcpy(strings[count_str], value);
+                      real_value = count_str;
+                      ++count_str;
+                    }
+                  else
+                    {
+                      real_value = pos;
+                    }
                 }
-                
-              {
-                if(buffer[i] == ',')
-                  {
-                    ++feature;
-                    value_pos = 0;
-                  }
-                else if (buffer[i] == '\n')
-                  {
-                    ++line;
-                    feature = 0;
-                    value_pos = 0;
-                  }
-              }
+
+              if(feature == features - 1)
+                {
+                  (*y)->data[line] = real_value;
+                }
+              else
+                {
+                  (*X)->data[line][feature] = real_value;
+                }
+
+              if (buffer[i] == '\n')
+                {
+                  ++line;
+                  feature = 0;
+                  value_pos = 0;
+                }
+              if(buffer[i] == ',')
+                {
+                  ++feature;
+                  value_pos = 0;
+                }
+
             }
         }
 
     }
   while (rv == sizeof(buffer));
 
-  X->print(X, 180);
-  y->print(y, 180);
   close(fd);
-  if(close_err == -1) fputs("Close failed!\n", __stdoutp);
+  if(close_err == -1) fputs("Close failed!\n", stdout);
 
   return 1;
 }
--- src/csv/csv_reader.h
+++ src/csv/csv_reader.h
@@ -8,6 +8,10 @@
  * Author: Georgi Korchakov
  */
 
-int read_csv(char* file_name, matrix_t* X, vector_t* y);
+int in_arr(char str_arr[100][40], char *str);
+
+int is_number(char *str);
+
+int read_csv(char* file_name, matrix_t **X, vector_t **y);
 
 #endif // ifndef _CSV_READER_H
